7926f9a1bd285079a41a8c755b06320595a297d4
keremec <k.safa.dirican@outlook.com>
2022-08-06 15:42:08 +0300

    Initial Commit

---
+++ Odev3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Odev3
+//
+//  Created by Kerem Safa Dirican on 6.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
